bd20b054f86ce577fcb6233a0c12d2d0b9674ca5
margaiwangara <margaiwangara@gmail.com>
2023-07-28 20:52:10 +0300

    feat: hash password

--- routers/auth.py
+++ routers/auth.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter
 from models import CreateUserRequest, User
+from passlib.context import CryptContext
+from starlette import status
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-@router.post("/auth/register")
+
+@router.post("/auth/register", status_code=status.HTTP_201_CREATED)
 async def create_user(user: CreateUserRequest):
+    hashed_password = bcrypt_context.hash(user.password)
+
     new_user = User(
        name=user.name,
        email=user.email,
-       password=user.password,
+       password=hashed_password,
        is_active=True,
        role=user.role,
     )
