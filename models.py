1aabb71893aa059131983084bbdb0a74b54bbab1
margaiwangara <margaiwangara@gmail.com>
2023-07-28 20:03:12 +0300

    feat: todo rest

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+from pydantic import BaseModel, Field
 
 
 class Todo(Base):
@@ -11,3 +12,20 @@ class Todo(Base):
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
 
+
+class TodoRequest(BaseModel):
+
+    title: str = Field(min_length=5, max_length=50)
+    description: str = Field(min_length=10, max_length=200)
+    priority: int = Field(ge=0)
+    complete: bool = False
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "Buy groceries",
+                "description": "Get milk, eggs, bread, and vegetables from the supermarket",
+                "priority": 3,
+                "complete": False
+            }
+        }
