5a7fda797ec28c49bda5779690bc5761f00b1207
margaiwangara <margaiwangara@gmail.com>
2023-07-28 20:35:12 +0300

    feat: user model

--- models.py
+++ models.py
@@ -1,6 +1,21 @@
+import datetime
+
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.sql import func
 from pydantic import BaseModel, Field
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+    created_at = Column(DateTime, default=func.now())
 
 
 class Todo(Base):
@@ -11,6 +26,7 @@ class Todo(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
 
 class TodoRequest(BaseModel):
